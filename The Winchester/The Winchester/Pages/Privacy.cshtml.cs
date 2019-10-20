a498e25de9fb15bf16840fe2f28a93c06399ba84
Josh Franklin <josh-franklin-@hotmail.co.uk>
2019-10-20 16:25:53 +0100

    Added ERD to document and created project for website

---
+++ The Winchester/The Winchester/Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace The_Winchester.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
