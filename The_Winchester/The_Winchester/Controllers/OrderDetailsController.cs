e03b09b6e0ec922612763a71603b9b80cb4a2e51
Josh Franklin <josh-franklin-@hotmail.co.uk>
2020-01-07 00:45:42 +0000

    Remade project completely, recurring EF bug. Now working

---
+++ The_Winchester/The_Winchester/Controllers/OrderDetailsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using The_Winchester;
+
+namespace The_Winchester.Controllers
+{
+    public class OrderDetailsController : Controller
+    {
+        private readonly ISAD251_JFranklinContext _context;
+
+        public OrderDetailsController(ISAD251_JFranklinContext context)
+        {
+            _context = context;
+        }
+
+        // GET: OrderDetails
+        public async Task<IActionResult> Index()
+        {
+            var iSAD251_JFranklinContext = _context.OrderDetails.Include(o => o.Order).Include(o => o.Product);
+            return View(await iSAD251_JFranklinContext.ToListAsync());
+        }
+
+        // GET: OrderDetails/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderDetails = await _context.OrderDetails
+                .Include(o => o.Order)
+                .Include(o => o.Product)
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+            if (orderDetails == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderDetails);
+        }
+
+        // GET: OrderDetails/Create
+        public IActionResult Create()
+        {
+            ViewData["OrderId"] = new SelectList(_context.Order, "OrderId", "OrderId");
+            ViewData["ProductId"] = new SelectList(_context.Product, "ProductId", "ProdCategory");
+            return View();
+        }
+
+        // POST: OrderDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("OrderId,ProductId,Quantity")] OrderDetails orderDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(orderDetails);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OrderId"] = new SelectList(_context.Order, "OrderId", "OrderId", orderDetails.OrderId);
+            ViewData["ProductId"] = new SelectList(_context.Product, "ProductId", "ProdCategory", orderDetails.ProductId);
+            return View(orderDetails);
+        }
+
+        // GET: OrderDetails/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderDetails = await _context.OrderDetails.FindAsync(id);
+            if (orderDetails == null)
+            {
+                return NotFound();
+            }
+            ViewData["OrderId"] = new SelectList(_context.Order, "OrderId", "OrderId", orderDetails.OrderId);
+            ViewData["ProductId"] = new SelectList(_context.Product, "ProductId", "ProdCategory", orderDetails.ProductId);
+            return View(orderDetails);
+        }
+
+        // POST: OrderDetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("OrderId,ProductId,Quantity")] OrderDetails orderDetails)
+        {
+            if (id != orderDetails.OrderId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(orderDetails);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderDetailsExists(orderDetails.OrderId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OrderId"] = new SelectList(_context.Order, "OrderId", "OrderId", orderDetails.OrderId);
+            ViewData["ProductId"] = new SelectList(_context.Product, "ProductId", "ProdCategory", orderDetails.ProductId);
+            return View(orderDetails);
+        }
+
+        // GET: OrderDetails/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderDetails = await _context.OrderDetails
+                .Include(o => o.Order)
+                .Include(o => o.Product)
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+            if (orderDetails == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderDetails);
+        }
+
+        // POST: OrderDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var orderDetails = await _context.OrderDetails.FindAsync(id);
+            _context.OrderDetails.Remove(orderDetails);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderDetailsExists(int id)
+        {
+            return _context.OrderDetails.Any(e => e.OrderId == id);
+        }
+    }
+}
---
+++ The_Winchester/The_Winchester/Models/ISAD251_JFranklinContext.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace The_Winchester
+{
+    public partial class ISAD251_JFranklinContext : DbContext
+    {
+        public ISAD251_JFranklinContext()
+        {
+        }
+
+        public ISAD251_JFranklinContext(DbContextOptions<ISAD251_JFranklinContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<MonthlySales> MonthlySales { get; set; }
+        public virtual DbSet<Order> Order { get; set; }
+        public virtual DbSet<OrderDetails> OrderDetails { get; set; }
+        public virtual DbSet<Product> Product { get; set; }
+        public virtual DbSet<Sales> Sales { get; set; }
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=socem1.uopnet.plymouth.ac.uk;Database=ISAD251_JFranklin;User Id=JFranklin;Password=ISAD251_22203114");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MonthlySales>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToView("MonthlySales");
+
+                entity.Property(e => e.OrderId).HasColumnName("OrderID");
+
+                entity.Property(e => e.TotalCost).HasColumnType("decimal(38, 0)");
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.HasKey(e => e.OrderId)
+                    .HasName("Order_pk")
+                    .IsClustered(false);
+
+                entity.Property(e => e.OrderId).HasColumnName("order_id");
+
+                entity.Property(e => e.Completed).HasColumnName("completed");
+
+                entity.Property(e => e.Preparing).HasColumnName("preparing");
+
+                entity.Property(e => e.TableNumber).HasColumnName("table_number");
+
+                entity.Property(e => e.TimeCompleted)
+                    .HasColumnName("time_completed")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.TimeOrdered)
+                    .HasColumnName("time_ordered")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.UserId).HasColumnName("user_id");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Order)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("Order_User_user_id_fk");
+            });
+
+            modelBuilder.Entity<OrderDetails>(entity =>
+            {
+                entity.HasKey(e => new { e.OrderId, e.ProductId })
+                    .HasName("Order_Details_pk")
+                    .IsClustered(false);
+
+                entity.ToTable("Order_Details");
+
+                entity.Property(e => e.OrderId).HasColumnName("order_id");
+
+                entity.Property(e => e.ProductId).HasColumnName("product_id");
+
+                entity.Property(e => e.Quantity).HasColumnName("quantity");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderDetails)
+                    .HasForeignKey(d => d.OrderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Order_Details_Order_order_Id_fk");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.OrderDetails)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Order_Details_Product_product_id_fk");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(e => e.ProductId)
+                    .HasName("Product_pk")
+                    .IsClustered(false);
+
+                entity.Property(e => e.ProductId).HasColumnName("product_id");
+
+                entity.Property(e => e.InUse).HasColumnName("in_use");
+
+                entity.Property(e => e.ProdCategory)
+                    .IsRequired()
+                    .HasColumnName("prod_category");
+
+                entity.Property(e => e.ProdDesc).HasColumnName("prod_desc");
+
+                entity.Property(e => e.ProdName)
+                    .IsRequired()
+                    .HasColumnName("prod_name");
+
+                entity.Property(e => e.ProdPrice)
+                    .HasColumnName("prod_price")
+                    .HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.Stock).HasColumnName("stock");
+            });
+
+            modelBuilder.Entity<Sales>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToView("Sales");
+
+                entity.Property(e => e.OrderId).HasColumnName("OrderID");
+
+                entity.Property(e => e.TotalCost).HasColumnType("decimal(38, 0)");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("User_pk")
+                    .IsClustered(false);
+
+                entity.Property(e => e.UserId).HasColumnName("user_id");
+
+                entity.Property(e => e.Admin).HasColumnName("admin");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasColumnName("first_name");
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasColumnName("last_name");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
